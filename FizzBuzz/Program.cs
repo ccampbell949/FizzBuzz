9776a7a20ce6205669366336bb82053193562548
Connor Campbell <connor1990@hotmail.co.uk>
2024-11-27 10:57:40 +0000

    Added FizzBuzz Logic

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+int n = 50;
+
+var list = new List<string>();
+
+
+for (int i = 1; i <= n; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        list.Add("FizzBuzz");
+    }
+
+    else if (i % 3 == 0)
+    {
+        list.Add("Fizz");
+        //Console.WriteLine("Fizz");
+    }
+
+    else if (i % 5 == 0)
+    {
+        list.Add("Buzz");
+        //Console.WriteLine("Buzz");
+    }
+
+    else
+    {
+        list.Add(i.ToString());
+        //Console.WriteLine(i.ToString());
+    }
+}
+
+list.ForEach(i => Console.Write(i + " "));
+
+
+
